13ec4d8c261f59790f40ee2d1c82822c580edf28
Gabriel da Silva Rodrigues <gabriel.rodrigues@salux.com.br>
2025-11-17 16:49:21 -0300

    more css styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import pokebola from './assets/pokebola.png';
+import lupa from './assets/lupa.png'
 import './App.css';
 import { useState, useEffect } from "react";
 import Card from './components/Card';
@@ -21,20 +22,25 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center bg-[#2b2b2b] bg-[repeating-linear-gradient(135deg,rgba(255,255,255,0.03)_0px,rgba(255,255,255,0.03)_40px,transparent_40px,transparent_110px)]
+    <div className='flex flex-col items-center bg-[#2b2b2b] bg-[repeating-linear-gradient(135deg,rgba(255,255,255,0.03)_0px,rgba(255,255,255,0.03)_40px,transparent_40px,transparent_110px)]
       bg-[repeating-linear-gradient(135deg,rgba(255,255,255,0.02)_0px,rgba(255,255,255,0.02)_4px,transparent_4px,transparent_40px)] bg-[radial-gradient(circle_at_20%_30%,rgba(255,255,255,0.05)_2px,transparent_3px)]
       bg-[radial-gradient(circle_at_70%_60%,rgba(255,255,255,0.05)_2px,transparent_3px)] bg-[radial-gradient(circle_at_40%_80%,rgba(255,255,255,0.05)_2px,transparent_3px)] bg-[radial-gradient(circle_at_85%_20%,rgba(255,255,255,0.05)_2px,transparent_3px)]
-      bg-blend-normal">
-      <div className="w-[70%] bg-white">
-        <h1 className=''>Pokédex</h1>
-        <div className="bg-gray-800 text-white p-5 flex flex-col">
-          <label>Nome ou número</label>
-          <div className="mt-2">
-            <input /> 
-            <button>Pesquisar</button>
+      bg-blend-normal'>
+      <div className='w-[70%] bg-white'>
+        <div className='flex items-center justify-center p-2'>
+          <img src={pokebola} title='pokebola' className='w-20 h-20'/>
+          <h1 className='text-[#919191] text-4xl'>Pokédex</h1>
+        </div>
+        <div className='bg-gray-800 p-5 flex flex-col'>
+          <label className='text-2xl font-montserrat text-white'>Nome ou número</label>
+          <div className='mt-2 flex '>
+            <input className='rounded-md me-2'/> 
+            <button className='bg-red-600 p-1 rounded-md'>
+              < img src={lupa}/>
+            </button>
           </div>
         </div>
-        <div className="flex flex-wrap justify-center">
+        <div className='flex flex-wrap justify-center'>
           {
             data.map((item, index) => <Card pokemon={item} index={index}/>)
           }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,14 @@ function Card({pokemon, index}) {
     const id = index + 1;
     const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
     return (
-        <div className="flex-col items-center shadow-lg m-2 rounded-lg p-2 bg-[linear-gradient(to_bottom,_#f8f8f8,_#efefef),_repeating-linear-gradient(45deg,_rgba(0,0,0,0.03)_0px,_rgba(0,0,0,0.03)_1px,_transparent_1px,_transparent_8px)] bg-blend-overlay">
+        <div className="flex-col items-center shadow-lg m-2 rounded-lg p-2 cursor-pointer
+        bg-[linear-gradient(to_bottom,_#f8f8f8,_#efefef),_repeating-linear-gradient(45deg,_rgba(0,0,0,0.03)_0px,_rgba(0,0,0,0.03)_1px,_transparent_1px,_transparent_8px)] bg-blend-overlay">
             <img
                 src={image}
                 alt="Bulbasaur"
                 className="w-40 h-40 object-cover"
             />
-            <p className="text-center">{pokemon?.name}</p>
+            <p className="text-center text-2xl font-montserrat font-bold tracking-wide text-cyan-800">{pokemon?.name}</p>
         </div>
     )
 }
